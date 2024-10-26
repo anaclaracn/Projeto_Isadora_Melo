feadf87ff6e37b9c1107110910319e41cf530339
Isadora Melo <isadora.cunha@estudante.ufla.br>
2024-10-26 19:12:02 -0300

    Criada a classe C

---
+++ src/C.java
@@ -0,0 +1,26 @@
+package src;
+
+public class C {
+    private String C1;
+    private int C2;
+
+    public String getC1() {
+        return C1;
+    }
+    public void setC1(String c1) {
+        C1 = c1;
+    }
+    public int getC2() {
+        return C2;
+    }
+    public void setC2(int c2) {
+        C2 = c2;
+    }
+    
+    public void MA1() {
+        System.out.println("MA1");
+    }
+    public void MA2() {
+        System.out.println("MA2");
+    }
+}
